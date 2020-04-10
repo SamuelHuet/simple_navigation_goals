c78bd4ec075ab3142c7d367165795f7b36909f98
Samuel Huet <contact@huetsamuel.fr>
2020-04-10 10:04:37 +0200

    syntax correction
    Signed-off-by: Samuel Huet <contact@huetsamuel.fr>

--- scripts/simple_navigation_goals.py
+++ scripts/simple_navigation_goals.py
@@ -4,10 +4,13 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-class simple_navigation_goals:
+class SimpleNavigationGoals:
+
+    def __init__(self):
+        pass
 
     def go_to(self, x, y, theta):
-        rospy.init_node('movebase_client_py')
+        # rospy.init_node('movebase_client_py')
         client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         client.wait_for_server()
 
@@ -15,18 +18,19 @@ class simple_navigation_goals:
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
 
-         goal.target_pose.pose.position.x = x
-         goal.target_pose.pose.position.y = y
-         goal.target_pose.pose.orientation.w = theta
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+        goal.target_pose.pose.orientation.w = theta
 
-         client.send_goal(goal)
-         wait = client.wait_for_result()
+        client.send_goal(goal)
+        wait = client.wait_for_result()
 
-         if not wait:
-             rospy.logerr("Action server not available!")
-             rospy.signal_shutdown("Action server not available!")
-         else:
-             return client.get_result()
+        if not wait:
+            rospy.logerr("Action server not available!")
+            rospy.signal_shutdown("Action server not available!")
+        # else:
+            # return client.get_result()
+
 
     def _shutdown(self):
         rospy.signal_shutdown("Shutdown!")
